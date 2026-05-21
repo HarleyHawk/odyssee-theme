eab6fd4d20661f5e02123b5c1ac9301729431c46
HarleyHawk <harleylmalakian@outlook.com>
2026-05-20 21:00:01 -0300

    feat: add About Me link to main navigation and update header styles for responsive alignment

--- header.php
+++ header.php
@@ -111,6 +111,9 @@
                 <a href="<?php echo esc_url( home_url( '/#impressos' ) ); ?>" data-key="impressos">Impressos</a>
                 <a href="<?php echo esc_url( home_url( '/servicos' ) ); ?>" data-key="servicos" class="nav-highlight">Serviços</a>
                 <a href="<?php echo esc_url( home_url( '/posts' ) ); ?>" data-key="todos-posts" class="nav-highlight">Blog</a>
+                <a href="<?php echo esc_url( home_url( '/sobre-mim/' ) ); ?>" class="nav-highlight" aria-label="Sobre Mim">
+                    <svg class="icon-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg> <span data-key="sobre-mim">Sobre Mim</span>
+                </a>
             </nav>
 
             <!-- Ações secundárias (FAQ e configurações) -->
